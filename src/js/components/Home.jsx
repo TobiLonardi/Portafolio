b9885ece5d24f280037bd96c30c4d0b4bf247b84
Tobias Lonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-09-11 19:50:59 +0000

    Header avanzado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,21 +6,20 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
+		<div className="container">
+			<div className="text-center d-flex flex-column justify-content-center align-items-start half-screen">
+				<p className="ms-3">Updated</p>
+				<div className="d-flex justify-content-start align-items-start">
+					<h1 className="ms-2">PORTFOLIO</h1>
+					<p class="border rounded-5 bg-warning my-2 mx-2  px-4">2025</p>
+				</div>
+				<div className="d-flex justify-content-center align-items-center w-50">
+					<p className="col-3"> Web Developer</p>
+					<hr class="my-4 col-6 mx-auto  border-1 opacity-75 border-secondary" />
+					<p className="col-3">Tobias Lonardi</p>
+				</div>
+			</div>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
 		</div>
 	);
 };
