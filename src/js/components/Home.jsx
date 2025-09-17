9562f984cea4580e40ba4e08ba98b2b0bb4a1b14
Tobias Lonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-09-17 18:14:35 +0000

    paleta de colores agregado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -9,16 +9,16 @@ import unrealLogo from "../../img/Unreal-logo.png";
 const Home = () => {
 	return (
 		<div className="container">
-			<div className="border border-3 rounded w-100">
+			<div className="border border-3 background-gray rounded w-100">
 				<div className=" d-flex flex-column justify-content-center align-items-start half-screen">
 					<p className="ms-3">Updated</p>
 					<div className="d-flex justify-content-start align-items-start">
 						<h1 className="ms-2">PORTFOLIO</h1>
-						<p class="border rounded-5 bg-warning my-2 mx-2  px-4">2025</p>
+						<p class="border rounded-5 orange my-2 mx-2  px-4">2025</p>
 					</div>
 					<div className="d-flex justify-content-start align-items-center w-75  px-3">
 						<p className="col-2">Fullstack Developer</p>
-						<hr class="my-4 col-6 mx-auto  border-1 opacity-100 border-secondary bg-light border-white" />
+						<hr class="my-4 col-6 mx-auto border-1 opacity-100 border-secondary " />
 						<p className="col-4 px-3">Tobias Lonardi</p>
 					</div>
 				</div>
@@ -26,56 +26,56 @@ const Home = () => {
 
 			</div>
 			<div className="d-flex justify-content-center align-items-center my-3">
-				<h3 className="text-center  border rounded-4 bg-warning my-3 py-3 skills-title">Skills</h3>
+				<h3 className="text-center  border rounded-4 orange my-3 py-3 skills-title">Skills</h3>
 			</div>
 
-			<div className="container border border-4 border-dark-subtle rounded rounded my-3  d-flex text-center justify-content-center align-items-center p-0">
+			<div className="container border border-4 border-dark-subtle  rounded rounded my-3  d-flex text-center justify-content-center align-items-center p-0">
 				<div className="row w-100">
-					<div className="col-1 border border-3 border-dark d-flex align-items-center justify-content-center  skills p-0">
+					<div className="col-1 border border-3 border-dark background-gray d-flex align-items-center justify-content-center  skills p-0">
 						<p className="m-0">Python <i class="fa-brands fa-python"></i></p>
 
 					</div>
-					<div className="col-1 border border-3 border-dark d-flex align-items-center justify-content-center p-0 skills">
+					<div className="col-1 border border-3 border-dark d-flex background-gray align-items-center justify-content-center p-0 skills">
 						<p className="m-0 ">JavaScript <i class="fa-brands fa-js"></i></p>
 
 					</div>
-					<div className="col-1 border border-3 border-dark d-flex align-items-center justify-content-center p-0 skills">
+					<div className="col-1 border border-3 border-dark d-flex background-gray align-items-center justify-content-center p-0 skills">
 						<p className="m-0">HTML5 <i class="fa-brands fa-html5"></i></p>
 
 					</div>
-					<div className="col-1 border border-3 border-dark d-flex align-items-center justify-content-center skills">
+					<div className="col-1 border border-3 border-dark d-flex background-gray align-items-center justify-content-center skills">
 						<p className="m-0">CSS <i class="fa-brands fa-css"></i></p>
 
 					</div>
-					<div className="col-1 border border-3 border-dark d-flex align-items-center justify-content-center p-0 skills">
+					<div className="col-1 border border-3 border-dark d-flex background-gray align-items-center justify-content-center p-0 skills">
 						<p className="m-0">Bootstrap <i class="fa-brands fa-bootstrap"></i></p>
 
 					</div>
-					<div className="col-1 border border-3 border-dark d-flex align-items-center justify-content-center skills p-0">
+					<div className="col-1 border border-3 border-dark d-flex background-gray align-items-center justify-content-center skills p-0">
 						<p className="m-0">React.js <i class="fa-brands fa-react"></i></p>
 
 					</div>
-					<div className="col-1 border border-3 border-dark d-flex align-items-center justify-content-center skills">
+					<div className="col-1 border border-3 border-dark d-flex background-gray align-items-center justify-content-center skills">
 						<p className="m-0">Flask <i class="fa-solid fa-flask"></i></p>
 
 					</div>
-					<div className="col-1 border border-3 border-dark d-flex align-items-center justify-content-center skills">
+					<div className="col-1 border border-3 border-dark d-flex background-gray align-items-center justify-content-center skills">
 						<p className="m-0">SQL <i class="fa-solid fa-database"></i></p>
 
 					</div>
 
-					<div className="col-1 border border-3 border-dark d-flex align-items-center justify-content-center skills p-0">
+					<div className="col-1 border border-3 border-dark d-flex background-gray align-items-center justify-content-center skills p-0">
 						<p className="m-0">GitHub <i class="fa-brands fa-github"></i></p>
 
 					</div>
-					<div className="col-1 border border-3 border-dark d-flex align-items-center justify-content-center skills">
+					<div className="col-1 border border-3 border-dark d-flex background-gray align-items-center justify-content-center skills">
 						<p className="m-0">C#</p>
 					</div >
-					<div className="col-1 border border-3 border-dark d-flex align-items-center justify-content-center skills">
+					<div className="col-1 border border-3 border-dark background-gray d-flex align-items-center justify-content-center skills">
 						<p className="m-0">Unity <i class="fa-brands fa-unity"></i></p>
 
 					</div>
-					<div className="col-1 border border-3 border-dark d-flex align-items-center justify-content-center skills p-0">
+					<div className="col-1 border border-3 border-dark background-gray d-flex align-items-center justify-content-center skills p-0">
 						<p className="m-0">Unreal <img src={unrealLogo} alt="" className="others-logos" /></p>
 
 
@@ -84,7 +84,7 @@ const Home = () => {
 
 			</div>
 
-			<div className="border border-3 rounded w-100 my-3 p-3 d-flex justify-content-between align-items-center">
+			<div className="border border-3 rounded background-gray w-100 my-3 p-3 d-flex justify-content-between align-items-center">
 				<img src={profilePicture} alt="" className="w-25 h-25" />
 				<div class="card w-25 mx-3 border-0">
 					<div class="card-body  ">
